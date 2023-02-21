928304e6ea62ee7963020c04125953c3293731e3
ingkptn <kleww473@newschool.edu>
2023-02-21 04:20:45 -0500

    new

--- Project1/part2/script.js
+++ Project1/part2/script.js
@@ -5,7 +5,7 @@ dot16.addEventListener("click", function(){
     dot16.style.width = '2500px';
     dot16.style.height = '2000px';
     dot16.style.zIndex = '20';
-    page2.style.color='white';
+    page2.style.color='#F8F5F0';
     page2.style.zIndex='30';
     page2.style.opacity='1';
 
@@ -28,7 +28,7 @@ dot13.addEventListener("click", function(){
     dot13.style.width = '3000px';
     dot13.style.height = '3000px';
     dot13.style.zIndex = '20';
-    page2.style.color='white';
+    page2.style.color='#F8F5F0';
     page2.style.zIndex='30';
     page2.style.opacity='1';
 
