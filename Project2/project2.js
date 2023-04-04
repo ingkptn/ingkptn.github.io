1b4634908c3796df8e02d5e5e8955f8f381e6c8a
ingkptn <kleww473@newschool.edu>
2023-04-04 08:15:43 -0400

    final

--- Project2/project2.js
+++ Project2/project2.js
@@ -1,3 +1,64 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+};
+
+        function scrolldown(){
+            document.body.style.overflowY = 'scroll';
+            document.body.style.overflowX = 'hidden';
+            window.scrollTo({top: 1200, behavior: 'smooth'})
+        }
+
+        test.addEventListener("click", scrolldown);
+
+        function scrollright(){
+            document.body.style.overflowX = 'scroll';
+            document.body.style.overflowY = 'hidden';
+            window.scrollTo({left: 2000, behavior: 'smooth'})
+        }
+
+       aboutbutton.addEventListener("click", scrollright);
+
+       function scrollback(){
+            document.body.style.overflowX = 'scroll';
+            document.body.style.overflowY = 'hidden';
+            window.scrollTo({left: 0, behavior: 'smooth'})
+        }
+
+       homebutton.addEventListener("click", scrollback);
+
+       function scrollup(){
+            document.body.style.overflowY = 'scroll';
+            document.body.style.overflowX = 'hidden';
+            window.scrollTo({top: 0, behavior: 'smooth'})
+        }
+
+       returnbutton.addEventListener("click", scrollup);
+
+       
+   
+        var video1 = document.querySelector("#videoElement1");
+        var video2 = document.querySelector("#videoElement2");
+        var video3 = document.querySelector("#videoElement3");
+        var video4 = document.querySelector("#videoElement4");
+        var video5 = document.querySelector("#videoElement5");
+        var video6 = document.querySelector("#videoElement6");
+
+    if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+    video1.srcObject = stream;
+    video2.srcObject = stream;
+    video3.srcObject = stream;
+    video4.srcObject = stream;
+    video5.srcObject = stream;
+    video6.srcObject = stream;
+    })
+
+    .catch(function (err0r) {
+    console.log("Something went wrong!");
+    });
+    }
+
 fetch('project2.json')
     .then(response => response.json())
     .then(data => {
@@ -9,22 +70,27 @@ fetch('project2.json')
             const date = document.createElement('p');
             const time = document.createElement('p');
             const videodiv = document.createElement('div');
+            const video = document.createElement('video');
             const vid = document.createElement('p');
             const img = document.createElement('img');
             const overlay = document.createElement('img');
-            
+            const largescreen = document.createElement('img');
             
             date.textContent = item.Date;
             time.textContent = item.Time;
             vid.textContent = item.VideoLength;
             img.src = "assets/images/" + item.Photo + ".jpg";
-            overlay.src = "assets/background1.png"
+            video.src = "assets/videos/" + item.Video + ".mp4";
+            overlay.src = "assets/background1.png";
+            largescreen.src = "assets/img/" + item.Photo + ".jpg";
+            
 
                 textdiv.appendChild(date);
                 textdiv.appendChild(time);
                 videodiv.appendChild(vid);
                 div.appendChild(img);
                 div.appendChild(textdiv);
+                div.appendChild(video);
                 div.appendChild(videodiv);
                 div.appendChild(overlay);
                 grid.appendChild(div);
@@ -33,36 +99,139 @@ fetch('project2.json')
             overlay.classList.add("overlay");
             textdiv.classList.add("textdiv");
             videodiv.classList.add("videodiv");
+            video.classList.add("video");
 
+            if (item.Video == "") {
+                                img.style.opacity = "1";
+                                overlay.style.opacity = "0.1";
+                                video.style.display = "none"
+                                
+                            } else {
+                                img.style.display = "none";
+                                overlay.style.opacity = "0.2";
+                            }
 
-            // function filter() {
-            //     var element= document.getElementsByClassName('color1');
-                
-            //     for(var x=0;x<element.length;x++)
-            //     {element[x].classList.toggle('appear1');}
-
-// if (item.Invasionlevel == 1) {
-//                 img.style.opacity = "1";
-//                 overlay.style.opacity = "0.15";
-//                 // div.style.border = "1.5px solid greenyellow";
-                
-//             } else {
-//                 img.style.opacity = "0";
-//                 overlay.style.opacity = "0";
-//                 div.style.border = "0px";
-//                 textdiv.style.opacity = "0";
-//                 videodiv.style.opacity = "0";
-//             }
             
-        });
-        
+            
+                              
 
- 
+            
+
+            filter1.addEventListener("click",() => {
+                if (item.Invasionlevel == "1") {
+                    div.style.opacity = "1";
+                } else {
+                    div.style.opacity = "0";
+                    videogrid.style.display = "none";
+                }
+        });
+
+    filter2.addEventListener("click",() => {
+        if (item.Invasionlevel == "2") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "none";
+        }    
+});
+
+    filter3.addEventListener("click",() => {
+        if (item.Invasionlevel == "3") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter4.addEventListener("click",() => {
+        if (item.Invasionlevel == "4") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter5.addEventListener("click",() => {
+        if (item.Invasionlevel == "5") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter6.addEventListener("click",() => {
+        if (item.Invasionlevel == "6") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "none";
+        }
+    });
+
+
+    filter7.addEventListener("click",() => {
+        if (item.Invasionlevel == "7") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "none";
+        }
+    });
+
+
+    filter8.addEventListener("click",() => {
+        if (item.Invasionlevel == "8") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter9.addEventListener("click",() => {
+        if (item.Invasionlevel == "9") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter10.addEventListener("click",() => {
+        if (item.Invasionlevel == "10") {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = "0";
+            videogrid.style.display = "grid";
+        }
+    });
+
+    reset.addEventListener("click",() => {
+        if (item.Invasionlevel > 0 ) {
+            div.style.opacity = "1";
+            videogrid.style.display = "none";
+        }
     })
 
+
+        div.addEventListener('mouseenter', () => {
+            overlay.style.opacity = "0.05";
+            video.play();
+            });
+                          
+        div.addEventListener('mouseleave', () => {
+            overlay.style.opacity = "0.15"
+            video.pause();
+            });
+
+
+    })
+    
     
 
-
-
+    })
 
 
---
+++ Project2/script.js
@@ -0,0 +1,363 @@
+// JSON data
+
+fetch('project2.json')
+    .then(response => response.json())
+    .then(data => {
+        const grid = document.getElementById('grid-container');
+
+        data.forEach(item => {
+            const div = document.createElement('div');
+            const textdiv = document.createElement('div');
+            const date = document.createElement('p');
+            const time = document.createElement('p');
+            const videoinfo = document.createElement('div');
+            const video = document.createElement('video');
+            const vid = document.createElement('p');
+            const img = document.createElement('img');
+            const overlay = document.createElement('img');
+            
+            date.textContent = item.Date;
+            time.textContent = item.Time;
+            vid.textContent = item.VideoLength;
+            img.src = "assets/images/" + item.Photo + ".jpg";
+            video.src = "assets/videos/" + item.Video + ".mp4";
+            overlay.src = "assets/background1.png";
+            
+
+                textdiv.appendChild(date);
+                textdiv.appendChild(time);
+                videoinfo.appendChild(vid);
+                div.appendChild(img);
+                div.appendChild(textdiv);
+                div.appendChild(video);
+                div.appendChild(videoinfo);
+                div.appendChild(overlay);
+                grid.appendChild(div);
+
+            div.classList.add("container");
+            overlay.classList.add("overlay");
+            textdiv.classList.add("textdiv");
+            videoinfo.classList.add("videodiv");
+            video.classList.add("video");
+
+            if (item.Video == "") {
+                                img.style.opacity = "1";
+                                overlay.style.opacity = "0.1";
+                                video.style.display = "none"
+                                
+                            } else {
+                                img.style.display = "none";
+                                overlay.style.opacity = "0.2";
+                            }
+
+            
+    // FILTER SYSTEM
+
+    function filterinvasion(){
+        div.style.opacity="1";
+    invasionfilter.style.display = "flex";
+    invasionbutton.style.backgroundColor = "#50C878";
+    invasionbutton.style.color = "black";
+    mediabutton.style.backgroundColor = "black";
+    mediabutton.style.color = "#50C878";
+    mediafilter.style.display = "none";
+    videogrid.style.display = "none";
+    }       
+
+    invasionbutton.addEventListener('click',filterinvasion)
+           
+    function filtermedia(){
+        div.style.display ="inline-block";
+        mediabutton.style.backgroundColor = "#50C878";
+        mediabutton.style.color = "black";
+        invasionbutton.style.backgroundColor = "black";
+        invasionbutton.style.color = "#50C878";
+        mediafilter.style.display = "flex";
+        invasionfilter.style.display = "none";
+        videogrid.style.display = "none";
+        }       
+    
+        mediabutton.addEventListener('click',filtermedia)
+          
+
+    // FILTER BY MEDIA
+    filterimage.addEventListener("click",() => {
+        filterimage.style.backgroundColor= "#50C878";
+        filterimage.style.color= "black";
+        filtervideo.style.backgroundColor= "black";
+        filtervideo.style.color= "#50C878";
+        if (item.Video == ""){
+            div.style.opacity = "1";
+            
+        }
+        else {
+            div.style.opacity = "0"
+        }
+    })
+
+    filtervideo.addEventListener("click",() => {
+        filtervideo.style.backgroundColor= "#50C878";
+        filtervideo.style.color= "black";
+        filterimage.style.backgroundColor= "black";
+        filterimage.style.color= "#50C878";
+        if (item.Video == ""){
+            div.style.opacity = "0";
+        }
+        else {
+            div.style.opacity = "1";
+        }
+    })
+
+    // FILTER BY INVASION LEVEL
+    
+
+    filter1.addEventListener("click",() => {
+                if (item.Invasionlevel == "1") {
+                    // div.style.opacity = "1";
+                    div.style.display = "inline-block";
+                } else {
+                    // div.style.opacity = "0";
+                    div.style.display = "none";
+                    videogrid.style.display = "none";
+                }
+    });
+
+    filter2.addEventListener("click",() => {
+        if (item.Invasionlevel == "2") {
+            // div.style.opacity = "1";
+            div.style.display = "inline-block";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "none";
+        }    
+    });
+
+    filter3.addEventListener("click",() => {
+        if (item.Invasionlevel == "3") {
+            // div.style.opacity = "1";
+            div.style.display = "inline-block";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter4.addEventListener("click",() => {
+        if (item.Invasionlevel == "4") {
+            div.style.display = "inline-block";
+            // div.style.opacity = "1";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter5.addEventListener("click",() => {
+        if (item.Invasionlevel == "5") {
+            // div.style.opacity = "1";
+            div.style.display = "inline-block";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter6.addEventListener("click",() => {
+        if (item.Invasionlevel == "6") {
+            // div.style.opacity = "1";
+            div.style.display = "inline-block";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "none";
+        }
+    });
+
+
+    filter7.addEventListener("click",() => {
+        if (item.Invasionlevel == "7") {
+            // div.style.opacity = "1";
+            div.style.display = "inline-block";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "none";
+        }
+    });
+
+
+    filter8.addEventListener("click",() => {
+        if (item.Invasionlevel == "8") {
+            // div.style.opacity = "1";
+            div.style.display = "inline-block";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter9.addEventListener("click",() => {
+        if (item.Invasionlevel == "9") {
+            // div.style.opacity = "1";
+            div.style.display = "inline-block";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "none";
+        }
+    });
+
+    filter10.addEventListener("click",() => {
+        if (item.Invasionlevel == "10") {
+            div.style.opacity = "1";
+        } else {
+            // div.style.opacity = "0";
+            div.style.display = "none";
+            videogrid.style.display = "grid";
+        }
+    });
+
+
+    // RESET FILTERS
+
+
+    reset.addEventListener("click",() => {
+            div.style.opacity= "1";
+            div.style.display= "inline-block";
+            videogrid.style.display = "none";
+            // invasionfilter.style.display = "none";
+            // filter.style.display = "none";
+    })
+
+
+    // VIDEO PLAY ON HOVER
+
+        div.addEventListener('mouseenter', () => {
+            overlay.style.opacity = "0.05";
+            video.play();
+            });
+                          
+        div.addEventListener('mouseleave', () => {
+            overlay.style.opacity = "0.15"
+            video.pause();
+            });
+
+
+    })
+
+    })
+
+
+
+
+
+
+// NAVIGATION BUTTONS
+
+window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+};
+
+        function scrolldown(){
+            document.body.style.overflowY = 'scroll';
+            document.body.style.overflowX = 'hidden';
+            window.scrollTo({top: 1200, behavior: 'smooth'})
+        }
+
+        test.addEventListener("click", scrolldown);
+
+        function scrollright(){
+            document.body.style.overflowX = 'scroll';
+            document.body.style.overflowY = 'hidden';
+            window.scrollTo({left: 2000, behavior: 'smooth'})
+        }
+
+       aboutbutton.addEventListener("click", scrollright);
+
+       function scrollback(){
+            document.body.style.overflowX = 'scroll';
+            document.body.style.overflowY = 'hidden';
+            window.scrollTo({left: 0, behavior: 'smooth'})
+        }
+
+       homebutton.addEventListener("click", scrollback);
+
+       function scrollup(){
+            document.body.style.overflowY = 'scroll';
+            document.body.style.overflowX = 'hidden';
+            window.scrollTo({top: 0, behavior: 'smooth'})
+        }
+
+       returnbutton.addEventListener("click", scrollup);
+       cancelButton.addEventListener("click", scrollup);
+
+    
+
+    //    WARNING WINDOW POPUP
+
+
+       let gridcontainer = document.getElementById('grid-container');
+       let filter = document.getElementById('filter-system');
+
+       function proceed(){
+        warningButtons.style.display = "none";
+        loading.style.display = "block";
+        warningWindow.style.animationPlayState = "running";
+        gridcontainer.style.animationPlayState = "running";
+        filter.style.animationPlayState = "running";
+        warning.textContent = "Proceeding...";
+       }
+
+
+       proceedButton.addEventListener("click", proceed);
+
+
+
+
+    // WEBCAMS
+        var video1 = document.querySelector("#videoElement1");
+        var video2 = document.querySelector("#videoElement2");
+        var video3 = document.querySelector("#videoElement3");
+        var video4 = document.querySelector("#videoElement4");
+        var video5 = document.querySelector("#videoElement5");
+        var video6 = document.querySelector("#videoElement6");
+        var video7 = document.querySelector("#videoElement7");
+        var video8 = document.querySelector("#videoElement8");
+        var video9 = document.querySelector("#videoElement9");
+        var video10 = document.querySelector("#videoElement10");
+        var video11 = document.querySelector("#videoElement11");
+        var video12 = document.querySelector("#videoElement12");
+
+    if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+    video1.srcObject = stream;
+    video2.srcObject = stream;
+    video3.srcObject = stream;
+    video4.srcObject = stream;
+    video5.srcObject = stream;
+    video6.srcObject = stream;
+    video7.srcObject = stream;
+    video8.srcObject = stream;
+    video9.srcObject = stream;
+    video10.srcObject = stream;
+    video11.srcObject = stream;
+    video12.srcObject = stream;
+    })
+
+    .catch(function (err0r) {
+    console.log("Something went wrong!");
+    });
+    }
+
+
+
+
+
+
+   
