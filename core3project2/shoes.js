19ed08d8a510f4e0f5560e933ea8cfad3db19c8c
ingkptn <kleww473@newschool.edu>
2023-10-23 17:42:42 -0400

    new

--- core3project2/shoes.js
+++ core3project2/shoes.js
@@ -119,6 +119,30 @@ alice.addEventListener("click", function(){
 
     })
 
+    junglebook.addEventListener("click", function(){
+        jb.style.display = "block";
+
+    })
+
+    archLink.addEventListener("click", function(){
+        archetypes.style.display = "block";
+    })
+
+    archetypes.addEventListener("dblclick", function(){
+        arch1.style.display = "block";
+        arch2.style.display = "block";
+        arch3.style.display = "block";
+        arch4.style.display = "block";
+        arch5.style.display = "block";
+        arch6.style.display = "block";
+        arch7.style.display = "block";
+        arch8.style.display = "block";
+        arch9.style.display = "block";
+        arch10.style.display = "block";
+        arch11.style.display = "block";
+        arch12.style.display = "block";
+
+    })
 
  function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -134,6 +158,37 @@ alice.addEventListener("click", function(){
     resizeCanvas(5230, windowHeight);
 
   }
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  // Function to generate random positions
+  function getRandomPosition() {
+    const maxWidth = window.innerWidth - 200; // Adjust for div width
+    const maxHeight = window.innerHeight - 100; // Adjust for div height
+    const randomX = Math.floor(Math.random() * maxWidth);
+    const randomY = Math.floor(Math.random() * maxHeight);
+    return { left: randomX + 'px', top: randomY + 'px' };
+  }
+  
+  // Get all elements with the class "containers arch"
+  const containers = document.querySelectorAll('.containers.arch');
+  
+  // Assign a random color and position to each container
+  containers.forEach(function(container) {
+    const randomPosition = getRandomPosition();
+    container.style.backgroundColor = getRandomColor();
+    container.style.left = randomPosition.left;
+    container.style.top = randomPosition.top;
+  });
+
+
+
 
 //   const answerForm = document.getElementById("answerForm");
 // const answerInput = document.getElementById("answerInput");
