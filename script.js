a528f683829c18a44596ad351c242dc9d7cc6313
ingkptn <kleww473@newschool.edu>
2026-06-13 16:55:43 +0700

    menu

--- script.js
+++ script.js
@@ -3,31 +3,35 @@ document.addEventListener('DOMContentLoaded', () => {
   const dropdownMenu = document.getElementById('menu');
   const menuBg = document.getElementById('menu-bg');
 
-  menuButton.addEventListener('click', (event) => {
-    event.stopPropagation();
-
+  function showMenu() {
     dropdownMenu.style.display = 'block';
     menuBg.style.display = 'block';
+  }
+
+  function hideMenu() {
+    dropdownMenu.style.display = 'none';
+    menuBg.style.display = 'none';
+  }
+
+  menuButton.addEventListener('click', (event) => {
+    event.stopPropagation();
+    showMenu();
   });
+
+  menuBg.addEventListener('click', hideMenu);
 
   window.addEventListener('click', (event) => {
     if (
-      event.target !== menuButton &&
+      !menuButton.contains(event.target) &&
       !dropdownMenu.contains(event.target)
     ) {
       hideMenu();
     }
   });
 
-  // Hide menu when window becomes wider than 1500px
   window.addEventListener('resize', () => {
     if (window.innerWidth > 1500) {
       hideMenu();
     }
   });
-
-  function hideMenu() {
-    dropdownMenu.style.display = 'none';
-    menuBg.style.display = 'none';
-  }
 });
